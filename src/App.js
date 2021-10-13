3525d85b8a65ced8b7be03472b2c562ab4021e67
ankitt01 <ankitrawat0102@gmail.com>
2021-10-13 23:54:01 +0530

    api call using axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+
+const options = {
+  method: 'GET',
+  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  params: {
+    q: 'London,uk',
+    lat: '0',
+    lon: '0',
+    id: '2172797',
+    lang: 'null',
+    units: 'imperial',
+    mode: 'xml'
+  },
+  headers: {
+    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+    'x-rapidapi-key': 'bbb25c8f99mshcd089c39737ba8cp11cacajsn846eca9a794c'
+  }
+};
+
 
 function App() {
+
+
+  useEffect(() => {
+    axios
+    .request(options)
+    .then((res) => {
+      console.log(JSON.stringify(res.data))
+    })
+    .catch(err => {console.log(err)})
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
